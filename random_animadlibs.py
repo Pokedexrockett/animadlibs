07c94a2464f78c01801f1d3065304b39d62207a8
Dexter Crockett <dex429.dc@gmail.com>
2022-09-20 21:26:33 -0400

    change letter case in 'line'

--- random_animadlibs.py
+++ random_animadlibs.py
@@ -1,6 +1,6 @@
-from shows import jojos_bizzare_madlibventure, mad_hero_libademia, Onelib_Madpiece
+from shows import jojos_bizzare_madlibventure, mad_hero_libademia, onelib_madpiece
 import random
 
 if __name__ == "__main__":
-    m = random.choice([jojos_bizzare_madlibventure, mad_hero_libademia, Onelib_Madpiece])
+    m = random.choice([jojos_bizzare_madlibventure, mad_hero_libademia, onelib_madpiece])
     m.madlib()
