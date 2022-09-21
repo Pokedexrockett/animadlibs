945dd35380dda299f1efe305ec8c0eb9af45883b
Dexter Crockett <dex429.dc@gmail.com>
2022-09-20 20:57:32 -0400

    add new noun, verb, and adjetive

--- shows/jojos_bizzare_madlibventure.py
+++ shows/jojos_bizzare_madlibventure.py
@@ -2,16 +2,18 @@ def madlib():
     protag_name = input("Name: ")
     verb1 = input("Verb: ")
     verb2 = input("Verb: ")
+    verb3 = input("Verb: ")
     noun1 = input("Noun: ")
     noun2 = input("Noun: ")
     noun3 = input("Noun: ")
     noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
     adj1 = input("Adjetive: ")
     adj2 = input("Adjetive: ")
     pronoun = input("Pronoun: ")
 
     madlib = f"My name is {protag_name}. Ever since the day {pronoun} came into my family's lives, we have known nothing but {noun1} and {noun2}. \
     He has sent many {adj1} stand users after us, but with the {verb1} of Kakyoin, Avdol, Polnereff, Iggy, and my grandfather Joseph we will {verb2} him and his stand The {noun3}. \
-    It is nothing compared to the power of my {noun4} Platnum. Together we {adj2} to Egypt from Japan and beat anyone or thing in our way. We will save my mother."\
+    It is nothing compared to the {noun5} of my {noun4} Platnum. Together we {verb3} to Egypt from Japan and beat anyone or thing in our way. We will save my mother."\
 
     print(madlib)
--- shows/mad_hero_libademia.py
+++ shows/mad_hero_libademia.py
@@ -7,13 +7,13 @@ def madlib():
     noun2 = input("Noun: ")
     noun3 = input("Noun: ")
     noun4 = input("Noun: ")
-    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
     adj1 = input("Adjetive: ")
     adj2 = input("Adjetive: ")
     pronoun = input("Pronoun: ")
 
     madlib = f"My name is {protag_name}. For as long as I can {verb1}, I have wanted to be a {noun1} like All {verb2}. Unfortunately, I was born {adj1}less so that dream seemed impossible. \
-    But one day I met the {noun2} himself, All {noun4}, who {verb3} to me with his {adj1}, {pronoun} For All. Together with his {noun3} and the amazing education at UA {noun1} Academy. \
+    But one day I met the {noun2} himself, All {noun5}, who {verb3} to me with his {adj1}, {pronoun} For All. Together with his {noun3} and the amazing education at UA {noun1} Academy. \
     I know I can be a {adj2} {noun1} and truly live up to the {noun4} given to me. I won’t let you down."\
 
     print(madlib)
