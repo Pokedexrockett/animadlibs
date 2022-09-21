9e97d0efdf13e11d8870410cb157b1ca0455ea8c
Dexter Crockett <dex429.dc@gmail.com>
2022-09-20 21:05:20 -0400

    add verbs, nouns, and adjetives to one piece

--- shows/Onelib_Madpiece.py
+++ shows/Onelib_Madpiece.py
@@ -1,18 +1,21 @@
 def madlib():
-    cstand_user = input("Stand User: ")
-    verb = input("Verb: ")
+    protag_name = input("Name: ")
+    verb1 = input("Verb: ")
+    verb2 = input("Verb: ")
+    verb3 = input("Verb: ")
+    noun1 = input("Noun: ")
+    noun2 = input("Noun: ")
+    noun3 = input("Noun: ")
+    noun4 = input("Noun: ")
+    noun5 = input("Noun: ")
     adj1 = input("Adjetive: ")
     adj2 = input("Adjetive: ")
-    adj3 = input("Adjetive: ")
-    adj4 = input("Adjetive: ")
-    noun1 = input("Adjetive: ")
-    noun2 = input("Adjetive: ")
-    noun3 = input("Adjetive: ")
+    pronoun = input("Pronoun: ")
 
-    madlib = f"My name is Monkey D. Luffy. And I’m the man who will become King Of The Pirates! With the straw hat entrusted to me by the great pirate Shanks, \
-    I’m going to gather a mighty crew and head out to the grand line in search of the One Piece. \
-    Gol D. Roger, the original King Of The Pirates conquered the Grand Line and found the legendary treasure himself. His death marked the start of the great pirate era we live in today. \
-    There are many fierce pirates on the Grand Line so my crew and I will need to be strong. I have faith in us though. With their help and the power of my Gum-Gum fruit, \
+    madlib = f"My name is {protag_name}. And I’m the {noun1} who will become {noun2} Of The Pirates! With the straw {noun3} {verb1} to me by the {adj1} pirate Shanks, \
+    I’m going to {verb2} a {adj2} crew and head out to the {noun4} line in search of the One Piece. \
+    Gol D. Roger, the original {noun2} Of The Pirates {verb3}ed the {noun4} Line and found the legendary treasure himself. His death marked the start of the {adj1} pirate era we live in today. \
+    There are many fierce pirates on the {noun4} Line so my crew and I will need to be strong. I have {noun5} in us though. With their help and the power of my {pronoun}-{pronoun} fruit, \
     I know we will succeed! Onward!"\
 
     print(madlib)
--- shows/jojos_bizzare_madlibventure.py
+++ shows/jojos_bizzare_madlibventure.py
@@ -14,6 +14,6 @@ def madlib():
 
     madlib = f"My name is {protag_name}. Ever since the day {pronoun} came into my family's lives, we have known nothing but {noun1} and {noun2}. \
     He has sent many {adj1} stand users after us, but with the {verb1} of Kakyoin, Avdol, Polnereff, Iggy, and my grandfather Joseph we will {verb2} him and his stand The {noun3}. \
-    It is nothing compared to the {noun5} of my {noun4} Platnum. Together we {verb3} to Egypt from Japan and beat anyone or thing in our way. We will save my mother."\
+    It is nothing compared to the {noun5} of my {adj2} {noun4} Platnum. Together we {verb3} to Egypt from Japan and beat anyone or thing in our way. We will save my mother."\
 
     print(madlib)
